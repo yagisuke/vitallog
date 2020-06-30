70fd38fddbbdce224ab2805bd3bc37f65c6e8898
yagisuke <yagisuke.0209@gmail.com>
2020-06-30 18:00:28 +0900

    logger

--- lib/ga.ts
+++ lib/ga.ts
@@ -1,19 +1,22 @@
 import ReactGA from 'react-ga'
 
 export const initGA = () => {
+  console.log(`[initGA]`)
   ReactGA.initialize('UA-154562499-2')
 }
 
 export const logPageView = () => {
+  console.log(`[logPageView] ${window.location.pathname}`)
   ReactGA.set({ page: window.location.pathname })
   ReactGA.pageview(window.location.pathname)
 }
 
 export const logWebVitals = (metric: any) => {
-  console.log(JSON.stringify(metric))
+  console.log(`[logWebVitals] ${JSON.stringify(metric)}`)
   ReactGA.timing({
     category: 'Web Vitals',
     variable: metric.name,
     value: metric.name === 'CLS' ? metric.value * 1000 : metric.value,
+    label: window.location.pathname
   })
 }
